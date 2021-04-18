8d54440ad464dc6925ecc679558bd9994e028cbc
Rogelio Medina <work@Rogelios-MacBook-Pro.local>
2021-04-18 01:43:28 -0500

    added material-ui to the dependencies and checked correct installation with some extra Containers (App.jsx)

---
+++ src/containers/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import "../assets/styles/containers/App.scss";
+
+function App() {
+  return <Button color="primary">Material-UI</Button>;
+}
+
+export default App;
