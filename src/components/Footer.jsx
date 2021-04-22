8e08fe36d54111fa098a2c888f0198221d8adf64
Rogelio Medina <rogelio.medina.cs@gmail.com>
2021-04-21 22:04:03 -0500

    login and footer: css and html finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,19 @@ import "../assets/styles/components/Footer.scss";
 
 const Footer = () => (
   <footer className="footer">
-    <a href="/">Terminos de uso</a>
-    <a href="/">Declaración de privacidad</a>
-    <a href="/">Centro de ayuda</a>
+    <div className="footer__decoration--up" />
+    <section className="footer__information">
+      <a href="/">Cambios, devoluciones y cancelaciones</a>
+      <a href="/">Preguntas frecuentes</a>
+      <a href="/">Ventas por teléfono</a>
+    </section>
+    <div className="footer__decoration--low">
+      <p>
+        Precio en rojo aplica el descuento más alto, las promociones pueden
+        diferir de las publicadas en tienda
+      </p>
+      <p>2021 Liverpool / Todos los derechos reservados D.R. ®</p>
+    </div>
   </footer>
 );
 
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+
 import "../assets/styles/containers/Login.scss";
 
 const Login = () => {
+  const [state, setState] = React.useState({ checked: false });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
   return (
     <section className="login">
+      <section className="login__banner">
+        <h1 className="login__banner--title"></h1>
+      </section>
       <section className="login__container">
+        <p className="login__container--division">
+          <span>Ingresa tus datos</span>
+        </p>
         <form className="login__container--form" noValidate autoComplete="off">
           <TextField
             className="input"
@@ -19,8 +35,25 @@ const Login = () => {
             label="Celular"
             variant="outlined"
           />
-          <button className="button">Button</button>
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={state.checked}
+                  onChange={handleChange}
+                  name="checked"
+                />
+              }
+              label="¿Eres tarjeta habiente?"
+            />
+          </FormGroup>
+          <button className="button">Entrar</button>
         </form>
+        <section className="login__container--terms">
+          <p>
+            <a>Política de privacidad</a> y <a>Términos y condiciones</a>
+          </p>
+        </section>
       </section>
     </section>
   );
