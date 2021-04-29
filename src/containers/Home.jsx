2f4a7423de2750faeb8c42003d18951c88c84ec5
francisco <faae00@hotmail.com>
2021-04-28 20:47:26 -0500

    se lee la base de datos de las promociones

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -7,14 +7,38 @@ import CarouselItemInfo from "../components/carouselNo-1/CarouselItemInfo.jsx";
 import CarouselSmall from "../components/carsouleNo-2/CarouselSmall.jsx";
 import CarouselItemSmall from "../components/carsouleNo-2/CarouselItemSmall.jsx";
 import "../assets/styles/containers/Home.scss";
+import { database } from "../firebase.js"
+import { arrayOf, element } from "prop-types";
 
 const Home = ({ cards, promos, user }) => {
+
+  
   const isCardOwner = () => {
     if (Object.keys(user).length > 0) {
       return user.checked;
     }
     return false;
   };
+
+  //database.collection(firstCollections + '/' + docId + '/' + subCollection).get()
+  /*database.collection("Promociones_General")
+  .doc("Departamento").collection("Zapateria").get()
+  .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+          console.log(doc.id, " => ", doc.data());
+      });
+  });
+  */
+  var array = ["Zapateria", "Linea_Blanca"];
+  var i;
+  for(i = 0; i < array.length; i++){
+    database.collection('Promociones_General/' + 'Departamento' + '/' + array[i]).get().then((subCollectionSnapshot) => {
+      subCollectionSnapshot.forEach((subDoc) => {
+          console.log(subDoc.data());
+      });
+    });
+  }
+
   return (
     <div className="Home">
       {!isCardOwner() && (
